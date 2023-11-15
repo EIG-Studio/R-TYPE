eb469c1420c49917e64753aa7ab25eb75184674c
Alrick <alrick.rubio@epitech.eu>
2023-11-15 13:56:54 +0100

    [ADD] clang-format, clang-tidy

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,21 @@
 #include <SFML/Graphics.hpp>
 
-int main() {
-  auto window = sf::RenderWindow{{800, 600}, "R-TYPE"};
-  window.setFramerateLimit(144);
+int main()
+{
+    auto window = sf::RenderWindow{{800, 600}, "R-TYPE"};
+    window.setFramerateLimit(144);
 
-  while (window.isOpen()) {
-    for (auto event = sf::Event{}; window.pollEvent(event);) {
-      if (event.type == sf::Event::Closed) {
-        window.close();
-      }
+    while (window.isOpen())
+    {
+        for (auto event = sf::Event{}; window.pollEvent(event);)
+        {
+            if (event.type == sf::Event::Closed)
+            {
+                window.close();
+            }
+        }
+
+        window.clear();
+        window.display();
     }
-
-    window.clear();
-    window.display();
-  }
 }
