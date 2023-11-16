293bcea2c3d94484bb0bf99a699cbcfe49685434
JulesTrolle <jules.trolle@epitech.eu>
2023-11-16 15:11:28 +0100

    [DEL] com in main

--- src/main.cpp
+++ src/main.cpp
@@ -37,41 +37,6 @@ int main()
     ChoiceMenu choiceMenu;
     Music      music;
 
-    // Sprite samuraiLogo;
-    // Sprite samuraiTitle;
-    // Music  music;
-    // if (!samuraiLogo
-    //          .load("../assets/MainMenu/samuraiLogo.png", sf::Vector2u(260, 299), window.getSize().x / 2 - 260 / 2, 50, 128, 147))
-    //     return -1;
-    // if (!samuraiTitle.load("../assets/MainMenu/samuraiTitle.png",
-    //                        sf::Vector2u(395, 53),
-    //                        window.getSize().x / 2 - 395 / 2,
-    //                        window.getSize().y / 1.5 - 53 / 2,
-    //                        128,
-    //                        18))
-    //     return -1;
-    //
-    //
-    // Button startButton(window,
-    //                    sf::Vector2f(200, 50),
-    //                    sf::Vector2f(window.getSize().x / 2 - 100, window.getSize().y / 2 + 160),
-    //                    sf::Color::Black,
-    //                    sf::Color::White,
-    //                    2.0f,
-    //                    "Play",
-    //                    font,
-    //                    20);
-    //
-    // Button exitButton(window,
-    //                   sf::Vector2f(200, 50),
-    //                   sf::Vector2f(window.getSize().x / 2 - 100, window.getSize().y / 2 + 230),
-    //                   sf::Color::Black,
-    //                   sf::Color::White,
-    //                   2.0f,
-    //                   "Exit",
-    //                   font,
-    //                   20);
-
     while (window.isOpen())
     {
         sf::Event event{};
