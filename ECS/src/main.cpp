9efc522f64856d3201a9cde62761fd68b8d7e31c
JulesTrolle <jules.trolle@epitech.eu>
2023-11-28 13:25:53 +0100

    [ADD] CMAKE in serveur and ECS

--- ECS/src/main.cpp
+++ ECS/src/main.cpp
@@ -1,1 +1,13 @@
- 
+/*
+** EPITECH PROJECT, 2023
+** R-TYPE
+** File description:
+** main
+*/
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello world!" << std::endl;
+    return 0;
+}
