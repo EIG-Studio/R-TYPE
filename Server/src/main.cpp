14f5a09e2c029b2cbe12496d15da2e51c3a931a6
Leandre17 <leandre.godet@epitech.eu>
2023-12-06 15:10:04 +0100

    [add] start of dlopen

--- Server/src/main.cpp
+++ Server/src/main.cpp
@@ -8,33 +8,22 @@
 #include "server.hpp"
 #include <dlfcn.h>
 
-// int main()
-// {
-    
-//     try {
-//         Server server;
-//         server.startListening();
-//     } catch (std::exception& e) {
-//         std::cerr << e.what() << std::endl;
-//         return 1;
-//     }
-
-//     return 0;
-// }
-
 int main()
 {
     void* libraryHandle = dlopen("GameEngine/build/samurai_ecs.so", RTLD_LAZY);
+
     if (!libraryHandle) {
         std::cerr << "Failed to load the shared library: " << dlerror() << std::endl;
+        return 84;
+    }
+    try {
+        Server server;
+        server.startListening();
+    } catch (std::exception& e) {
+        std::cerr << e.what() << std::endl;
         return 1;
     }
 
-    // Use the loaded library
-    // ...
-
-    // Unload the shared library
     dlclose(libraryHandle);
-
     return 0;
 }
