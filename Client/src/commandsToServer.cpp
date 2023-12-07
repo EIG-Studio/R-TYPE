243bfae44e3934b5998a744000444cd2a93a9e17
Leandre17 <leandre.godet@epitech.eu>
2023-12-07 18:51:14 +0100

    [FIX] clang format

--- Client/src/commandsToServer.cpp
+++ Client/src/commandsToServer.cpp
@@ -9,8 +9,7 @@ std::string binaryToText(const std::string& binaryString)
 {
     std::string text;
 
-    for (size_t i = 0; i < binaryString.length(); i += 8)
-    {
+    for (size_t i = 0; i < binaryString.length(); i += 8) {
         std::string binaryGroup = binaryString.substr(i, 8);
 
         int decimalValue = std::bitset<8>(binaryGroup).to_ulong();
@@ -24,18 +23,14 @@ std::string binaryToText(const std::string& binaryString)
 
 void handleReceive(const boost::system::error_code& error, size_t len, boost::array<char, 2048>& recvBuf, std::string& mNewPos)
 {
-    if (!error && len >= 7)
-    {
+    if (!error && len >= 7) {
         std::string asciiString(binaryToText(recvBuf.data()));
         std::cout << asciiString << std::endl;
 
-        if (std::string(asciiString.data(), 7) == "NEW_POS")
-        {
+        if (std::string(asciiString.data(), 7) == "NEW_POS") {
             mNewPos = asciiString;
         }
-    }
-    else
-    {
+    } else {
         std::cerr << "Error in handleReceive: " << error.message() << std::endl;
     }
 }
@@ -48,46 +43,39 @@ void sendToServer(boost::asio::ip::udp::socket& socket, const std::string& msg)
 
 void asyncReceive(boost::asio::ip::udp::socket& socket, boost::array<char, 2048>& recvBuf, std::string& mNewPos)
 {
-    socket.async_receive(boost::asio::buffer(recvBuf),
-                         [&recvBuf, &mNewPos](auto&& pH1, auto&& pH2) {
-                             return handleReceive(std::forward<decltype(pH1)>(pH1),
-                                                  std::forward<decltype(pH2)>(pH2),
-                                                  recvBuf,
-                                                  mNewPos);
-                         });
+    socket.async_receive(boost::asio::buffer(recvBuf), [&recvBuf, &mNewPos](auto&& pH1, auto&& pH2) {
+        return handleReceive(std::forward<decltype(pH1)>(pH1), std::forward<decltype(pH2)>(pH2), recvBuf, mNewPos);
+    });
 }
 
 std::future<void> CommandsToServer::sendToServerAsync(std::string msg)
 {
-    return std::async(std::launch::async,
-                      [this, msg = std::move(msg)]
-                      {
-                          try
-                          {
-                              boost::asio::io_service ioService;
+    return std::async(std::launch::async, [this, msg = std::move(msg)] {
+        try {
+            boost::asio::io_service ioService;
 
-                              boost::asio::ip::udp::endpoint receiverEndpoint(boost::asio::ip::address::from_string(
-                                                                                  "12"
-                                                                                  "7."
-                                                                                  "0."
-                                                                                  "0."
-                                                                                  "1"),
-                                                                              7171);
+            boost::asio::ip::udp::endpoint receiverEndpoint(
+                boost::asio::ip::address::from_string(
+                    "12"
+                    "7."
+                    "0."
+                    "0."
+                    "1"),
+                7171);
 
-                              boost::asio::ip::udp::socket socket(ioService);
-                              socket.open(boost::asio::ip::udp::v4());
+            boost::asio::ip::udp::socket socket(ioService);
+            socket.open(boost::asio::ip::udp::v4());
 
-                              sendToServer(socket, msg);
+            sendToServer(socket, msg);
 
-                              boost::array<char, 2048> recvBuf{};
-                              asyncReceive(socket, recvBuf, m_newPos);
+            boost::array<char, 2048> recvBuf{};
+            asyncReceive(socket, recvBuf, m_newPos);
 
-                              ioService.run();
-                          } catch (std::exception& e)
-                          {
-                              std::cerr << e.what() << std::endl;
-                          }
-                      });
+            ioService.run();
+        } catch (std::exception& e) {
+            std::cerr << e.what() << std::endl;
+        }
+    });
 }
 
 std::string CommandsToServer::getNewPos() const
