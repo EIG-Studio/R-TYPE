eaca8dc46447636ffd8983c4702a1a027be50f13
Alrick <alrick.rubio@epitech.eu>
2023-12-20 16:02:40 +0100

    [ADD] addComoenent

--- GameEngine/src/Registry.cpp
+++ GameEngine/src/Registry.cpp
@@ -1,7 +1,7 @@
 #include "entities.hpp"
 
 template <typename T>
-void addComponent(Entity &entity, T component)
+void addComponent(Entity& entity, T component)
 {
-
+    entity.mComponents.push_back(component);
 }
