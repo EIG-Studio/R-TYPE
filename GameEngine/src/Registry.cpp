4fe582e02dc0f007e48361ead607aea16fbfc9ac
Alrick <alrick.rubio@epitech.eu>
2023-12-20 17:34:57 +0100

    [ADD] createEntity in regstry

--- GameEngine/src/Registry.cpp
+++ GameEngine/src/Registry.cpp
@@ -21,6 +21,15 @@ T& Registry::getComponent(Entity &entity, T component)
     return std::any_cast<T&>(entity.mComponents[component]);
 }
 
+Entity Registry::createEntity()
+{
+    assert(!m_entities.empty());
+
+    Entity id = m_entities.back();
+    m_entities.pop_back();
+    return id;
+}
+
 extern "C" Registry* factory()
 {
     return new Registry();
