49c4c5210a0151723cad1c64c2216042a1365fa8
Leandre17 <leandre.godet@epitech.eu>
2023-12-21 09:04:57 +0100

    [fix] Merge

--- Server/src/main.cpp
+++ Server/src/main.cpp
@@ -33,7 +33,9 @@ int main()
         factory();
 
         std::thread serverThread(&Server::startListening, &server);
+        std::thread serverThread2(&Server::startSending, &server);
         serverThread.join();
+        serverThread2.join();
     } catch (std::exception& e) {
         std::cerr << e.what() << std::endl;
         return 1;
