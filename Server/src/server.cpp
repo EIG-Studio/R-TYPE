0f154d850f3ba93f8145559f6d374e80510584dc
Alrick <alrick.rubio@epitech.eu>
2023-12-21 12:21:01 +0100

    [FIX] change 500ms to 10ms sleep in server

--- Server/src/server.cpp
+++ Server/src/server.cpp
@@ -37,7 +37,7 @@ void Server::startSending()
                 m_messages.pop_back();
             this->m_mutex.unlock();
         } else {
-            std::this_thread::sleep_for(std::chrono::milliseconds(500));
+            std::this_thread::sleep_for(std::chrono::milliseconds(010));
         }
     }
 }
