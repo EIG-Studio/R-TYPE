94b57f020a6e5cbc15d6f41ee8074b22497cdf56
4lrick <alrick.rubio@epitech.eu>
2023-12-22 14:56:06 +0100

    [RM] useless comment

--- Server/src/main.cpp
+++ Server/src/main.cpp
@@ -17,48 +17,14 @@
 int main()
 {
     try {
-        // sf::RenderWindow window(sf::VideoMode(800, 600), "R-TYPE");
-        // Registry registry = Registry();
-        // Entity entity;
-        // entity = registry.createEntity();
-        // entity = registry.addComponent(entity, Position(std::make_pair(200, 600)));
-        // // registry.addComponent(entity, Velocity());
-        // // registry.addComponent(entity, Speed(0));
-        // // registry.addComponent(entity, HealthPoint(100));
-        // // registry.addComponent(entity, Damage(0));
-        // // registry.addComponent(entity, Shooter());
-        // entity = registry.addComponent(entity, Renderer("../Client/assets/Cars/189_neutral.png"));
-        // entity = registry.addComponent(entity, Type(std::any_cast<EntityType>(Player)));
-        // // if (registry.hasComponent(entity, Position());
-        // while (window.isOpen()) {
-        //     sf::Event event{};
-        //     while (window.pollEvent(event)) {
-        //         if (event.type == sf::Event::Closed) {
-        //             window.close();
-        //         }
-        //     }
-        //
-        //     // Mettre à jour le jeu ici
-        //
-        //     // Effacer l'écran
-        //     window.clear();
-        //
-        //     // Dessiner les éléments du jeu ici
-        //
-        //     // Afficher la fenêtre
-        //
-        //
-        //     registry.systemsManager(window);
-        //     window.display();
-        // }
-        //
         Server server;
-
 
         std::thread serverThread(&Server::startListening, &server);
         std::thread serverThread2(&Server::startSending, &server);
+
         serverThread.join();
         serverThread2.join();
+
     } catch (std::exception& e) {
         std::cerr << e.what() << std::endl;
         return 1;
