9c52f3bc684ec89c2c83259a4dc09a4a05371ea4
Leandre17 <leandre.godet@epitech.eu>
2024-01-02 16:36:24 +0100

    [FIX] add message missing

--- Server/src/command.cpp
+++ Server/src/command.cpp
@@ -92,6 +92,9 @@ void Server::goUp(Registry& registry, std::string& command)
     positionComponent.setPosition(
         std::
             make_pair(positionComponent.getPosition().first, positionComponent.getPosition().second - 1 * speedComponent.getSpeed()));
+    std::string newPos = "NEW_POS " + std::to_string(positionComponent.getPosition().first) + " " +
+                         std::to_string(positionComponent.getPosition().second) + "\n";
+    addMessage(newPos);
 }
 
 void Server::goDown(Registry& registry, std::string& command)
@@ -109,6 +112,9 @@ void Server::goDown(Registry& registry, std::string& command)
     positionComponent.setPosition(
         std::
             make_pair(positionComponent.getPosition().first, positionComponent.getPosition().second + 1 * speedComponent.getSpeed()));
+    std::string newPos = "NEW_POS " + std::to_string(positionComponent.getPosition().first) + " " +
+                         std::to_string(positionComponent.getPosition().second) + "\n";
+    addMessage(newPos);
 }
 
 void Server::goRight(Registry& registry, std::string& command)
@@ -127,6 +133,9 @@ void Server::goRight(Registry& registry, std::string& command)
     positionComponent.setPosition(std::make_pair(
         positionComponent.getPosition().first + 1 * speedComponent.getSpeed(),
         positionComponent.getPosition().second));
+    std::string newPos = "NEW_POS " + std::to_string(positionComponent.getPosition().first) + " " +
+                         std::to_string(positionComponent.getPosition().second) + "\n";
+    addMessage(newPos);
 }
 
 void Server::goLeft(Registry& registry, std::string& command)
@@ -144,6 +153,9 @@ void Server::goLeft(Registry& registry, std::string& command)
     positionComponent.setPosition(std::make_pair(
         positionComponent.getPosition().first - 1 * speedComponent.getSpeed(),
         positionComponent.getPosition().second));
+    std::string newPos = "NEW_POS " + std::to_string(positionComponent.getPosition().first) + " " +
+                         std::to_string(positionComponent.getPosition().second) + "\n";
+    addMessage(newPos);
 }
 
 void Server::sendList()
