1f5318e4e219d80fe82be6c2f33f6c5975e5dda9
Alrick <alrick.rubio@epitech.eu>
2024-01-02 13:51:28 +0100

    [FIX] revert main

--- Server/src/main.cpp
+++ Server/src/main.cpp
@@ -16,43 +16,44 @@
 int main()
 {
     try {
-        sf::RenderWindow window(sf::VideoMode(800, 600), "R-TYPE");
-        Registry registry = Registry();
-        Entity entity;
-        entity = registry.createEntity();
-        entity = registry.addComponent(entity, Position(std::make_pair(200, 200)));
-        // registry.addComponent(entity, Velocity());
-        // registry.addComponent(entity, Speed(0));
-        // registry.addComponent(entity, HealthPoint(100));
-        // registry.addComponent(entity, Damage(0));
-        // registry.addComponent(entity, Shooter());
-        entity = registry.addComponent(entity, Renderer("../Client/assets/Cars/189.png"));
-        entity = registry.addComponent(entity, Type(std::any_cast<EntityType>(Player)));
-        window.setFramerateLimit(144);
-        // if (registry.hasComponent(entity, Position());
-        while (window.isOpen()) {
-            sf::Event event{};
-            while (window.pollEvent(event)) {
-                if (event.type == sf::Event::Closed) {
-                    window.close();
-                }
-            }
-
-            // Mettre à jour le jeu ici
-
-            // Effacer l'écran
-            window.clear();
-
-            // Dessiner les éléments du jeu ici
-
-            // Afficher la fenêtre
-
-
-            registry.systemsManager(window);
-            window.display();
-        }
+        // sf::RenderWindow window(sf::VideoMode(800, 600), "R-TYPE");
+        // Registry registry = Registry();
+        // Entity entity;
+        // entity = registry.createEntity();
+        // entity = registry.addComponent(entity, Position(std::make_pair(200, 200)));
+        // // registry.addComponent(entity, Velocity());
+        // // registry.addComponent(entity, Speed(0));
+        // // registry.addComponent(entity, HealthPoint(100));
+        // // registry.addComponent(entity, Damage(0));
+        // // registry.addComponent(entity, Shooter());
+        // entity = registry.addComponent(entity, Renderer("../Client/assets/Cars/189.png"));
+        // entity = registry.addComponent(entity, Type(std::any_cast<EntityType>(Player)));
+        // window.setFramerateLimit(144);
+        // // if (registry.hasComponent(entity, Position());
+        // while (window.isOpen()) {
+        //     sf::Event event{};
+        //     while (window.pollEvent(event)) {
+        //         if (event.type == sf::Event::Closed) {
+        //             window.close();
+        //         }
+        //     }
+        //
+        //     // Mettre à jour le jeu ici
+        //
+        //     // Effacer l'écran
+        //     window.clear();
+        //
+        //     // Dessiner les éléments du jeu ici
+        //
+        //     // Afficher la fenêtre
+        //
+        //
+        //     registry.systemsManager(window);
+        //     window.display();
+        // }
 
         Server server;
+        Registry registry;
 
         std::thread serverThread(&Server::startListening, &server, std::ref(registry));
         std::thread serverThread2(&Server::startSending, &server);
