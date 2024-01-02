26ea23c44b574d8df06cea99a0a734be70d148e0
Alrick <alrick.rubio@epitech.eu>
2024-01-02 19:31:40 +0100

    [ADD] ennmy in thread

--- Server/src/main.cpp
+++ Server/src/main.cpp
@@ -16,51 +16,17 @@
 int main()
 {
     try {
-        // sf::RenderWindow window(sf::VideoMode(800, 600), "R-TYPE");
-        // Registry registry = Registry();
-        // Entity entity;
-        // entity = registry.createEntity();
-        // entity = registry.addComponent(entity, Position(std::make_pair(200, 200)));
-        // // registry.addComponent(entity, Velocity());
-        // // registry.addComponent(entity, Speed(0));
-        // // registry.addComponent(entity, HealthPoint(100));
-        // // registry.addComponent(entity, Damage(0));
-        // // registry.addComponent(entity, Shooter());
-        // entity = registry.addComponent(entity, Renderer("../Client/assets/Cars/189.png"));
-        // entity = registry.addComponent(entity, Type(std::any_cast<EntityType>(Player)));
-        // window.setFramerateLimit(144);
-        // // if (registry.hasComponent(entity, Position());
-        // while (window.isOpen()) {
-        //     sf::Event event{};
-        //     while (window.pollEvent(event)) {
-        //         if (event.type == sf::Event::Closed) {
-        //             window.close();
-        //         }
-        //     }
-        //
-        //     // Mettre à jour le jeu ici
-        //
-        //     // Effacer l'écran
-        //     window.clear();
-        //
-        //     // Dessiner les éléments du jeu ici
-        //
-        //     // Afficher la fenêtre
-        //
-        //
-        //     registry.systemsManager(window);
-        //     window.display();
-        // }
-
         Server server;
         Registry registry;
-        server.createEnnemy(registry);
-        server.createEnnemy(registry);
+        std::thread ennemyThread(&Server::createEnnemy, &server, std::ref(registry));
+        std::thread ennemyThread2(&Server::createEnnemy, &server, std::ref(registry));
 
         std::thread serverThread(&Server::startListening, &server, std::ref(registry));
         std::thread serverThread2(&Server::startSending, &server);
         serverThread.join();
         serverThread2.join();
+        ennemyThread.join();
+        ennemyThread2.join();
     } catch (std::exception& e) {
         std::cerr << e.what() << std::endl;
         return 1;
