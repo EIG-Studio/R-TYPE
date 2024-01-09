ebe299ba4e31cc63e83d676c2b4760015dac0387
JulesTrolle <jules.trolle@epitech.eu>
2024-01-09 10:40:31 +0100

    [FIX] incorect argument in Button class

--- Client/include/button.hpp
+++ Client/include/button.hpp
@@ -99,7 +99,6 @@ class Button
 public:
     Button() = default;
     Button(
-        sf::RenderWindow& window,
         const sf::Vector2f& size,
         const sf::Vector2f& position,
         const sf::Color& fillColor,
@@ -175,7 +174,6 @@ public:
     m_window(window),
     m_font(mFont),
     m_playButton(
-        window,
         sf::Vector2f(200, 50),
         sf::Vector2f(window.getSize().x / 2 - 100, window.getSize().y / 2 - 60),
         sf::Color::Black,
@@ -185,7 +183,6 @@ public:
         m_font,
         20),
     m_settingsButton(
-        window,
         sf::Vector2f(200, 50),
         sf::Vector2f(window.getSize().x / 2 - 100, window.getSize().y / 2),
         sf::Color::Black,
@@ -195,7 +192,6 @@ public:
         m_font,
         20),
     m_retourButton(
-        window,
         sf::Vector2f(200, 50),
         sf::Vector2f(window.getSize().x / 2 + 150, window.getSize().y / 2 + 200),
         sf::Color::Black,
@@ -205,7 +201,6 @@ public:
         m_font,
         20),
     m_exitButton(
-        window,
         sf::Vector2f(200, 50),
         sf::Vector2f(window.getSize().x / 2 - 100, window.getSize().y / 2 + 60),
         sf::Color::Black,
@@ -241,7 +236,6 @@ private:
     void createButtons()
     {
         m_playButton = Button(
-            m_window,
             sf::Vector2f(200, 50),
             sf::Vector2f(m_window.getSize().x / 2 - 100, m_window.getSize().y / 2 - 60),
             sf::Color::Black,
@@ -252,7 +246,6 @@ private:
             20);
 
         m_settingsButton = Button(
-            m_window,
             sf::Vector2f(200, 50),
             sf::Vector2f(m_window.getSize().x / 2 - 100, m_window.getSize().y / 2),
             sf::Color::Black,
@@ -263,7 +256,6 @@ private:
             20);
 
         m_retourButton = Button(
-            m_window,
             sf::Vector2f(200, 50),
             sf::Vector2f(m_window.getSize().x / 2 + 150, m_window.getSize().y / 2 + 200),
             sf::Color::Black,
@@ -274,7 +266,6 @@ private:
             20);
 
         m_exitButton = Button(
-            m_window,
             sf::Vector2f(200, 50),
             sf::Vector2f(m_window.getSize().x / 2 - 100, m_window.getSize().y / 2 + 60),
             sf::Color::Black,
