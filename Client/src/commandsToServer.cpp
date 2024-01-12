12483fbc8eaee8052c4de7d80ba758ed2460ef28
Alrick <alrick.rubio@epitech.eu>
2024-01-12 10:46:36 +0100

    [ADD] clean commandToServer.cpp

--- Client/src/commandsToServer.cpp
+++ Client/src/commandsToServer.cpp
@@ -44,6 +44,127 @@ int randNb(int x, int y)
     return distrib(gen);
 }
 
+void updatePosition(Registry& registry, int id, int xPos, int yPos)
+{
+    try {
+        Entity entity = registry.getEntity(id);
+        if (registry.hasComponent(entity, Position{})) {
+            Position& entityPos = registry.getComponent(entity, Position{});
+            entityPos.setPosition(std::make_pair(xPos, yPos));
+            registry.setEntity(entity, id);
+        }
+    } catch (const std::exception& e) {
+        std::cerr << "Exception in updatePosition: " << e.what() << std::endl;
+    }
+}
+
+void updateHealth(Registry& registry, int id, int health)
+{
+    try {
+        Entity entity = registry.getEntity(id);
+        if (registry.hasComponent(entity, HealthPoint{})) {
+            HealthPoint& entityPos = registry.getComponent(entity, HealthPoint{});
+            entityPos.setHealthPoint(health);
+            registry.setEntity(entity, id);
+        }
+    } catch (const std::exception& e) {
+        std::cerr << "Exception in updateHealth: " << e.what() << std::endl;
+    }
+}
+
+void createPlayer(Registry& registry, int id, int xPos, int yPos, int healthPoint)
+{
+    if (registry.hasEntity(id))
+        return;
+    Entity player = registry.createEntityWithID(id);
+    player = registry.addComponent(player, Position(std::make_pair(xPos, yPos)));
+    player = registry.addComponent(player, HealthPoint(healthPoint));
+    player = registry.addComponent(player, Renderer("../Client/assets/Cars/189.png"));
+    player = registry.addComponent(player, Type(std::any_cast<EntityType>(Player)));
+    player = registry.addComponent(
+        player,
+        Size(std::make_pair(
+            103 / registry.getComponent(player, Renderer{}).getRenderer().getLocalBounds().width,
+            56.25 / registry.getComponent(player, Renderer{}).getRenderer().getLocalBounds().height)));
+    registry.setEntity(player, id);
+}
+
+void createEnemy(Registry& registry, int id, int xPos, int yPos)
+{
+    Entity enemy = registry.createEntityWithID(id);
+    enemy = registry.addComponent(enemy, Position(std::make_pair(xPos, yPos)));
+    enemy = registry.addComponent(enemy, Renderer("../Client/assets/Cars/cars/190.png"));
+    enemy = registry.addComponent(enemy, Type(std::any_cast<EntityType>(Enemy)));
+    enemy = registry.addComponent(
+        enemy,
+        Size(std::make_pair(
+            103 / registry.getComponent(enemy, Renderer{}).getRenderer().getLocalBounds().width,
+            56.25 / registry.getComponent(enemy, Renderer{}).getRenderer().getLocalBounds().height)));
+
+    Position enemyPos = registry.getComponent(enemy, Position{});
+    std::pair<int, int> pairPos = enemyPos.getPosition();
+
+    std::cout << "Enemy created pos: " << pairPos.first << " " << pairPos.second << '\n';
+}
+
+void createBoss(Registry& registry, int id, int xPos, int yPos)
+{
+    Entity boss = registry.createEntityWithID(id);
+    boss = registry.addComponent(boss, Position(std::make_pair(xPos, yPos)));
+    boss = registry.addComponent(boss, Renderer("../Client/assets/Boss/v-police.png"));
+    boss = registry.addComponent(boss, Type(std::any_cast<EntityType>(Enemy)));
+    boss = registry.addComponent(boss, Size(std::make_pair(2, 2)));
+
+    Position bossPos = registry.getComponent(boss, Position{});
+    std::pair<int, int> pairPos = bossPos.getPosition();
+
+    std::cout << "Boss created pos: " << pairPos.first << " " << pairPos.second << '\n';
+}
+
+void createPlayerProjectile(Registry& registry, int id, int xPos, int yPos)
+{
+    std::cout << "Player projectile created" << std::endl;
+    Entity playerProjectile = registry.createEntityWithID(id);
+    playerProjectile = registry.addComponent(playerProjectile, Position(std::make_pair(xPos, yPos)));
+    playerProjectile = registry.addComponent(playerProjectile, Renderer("../Client/assets/Cars/movement parts/thruster/flame.png"));
+    playerProjectile = registry.addComponent(playerProjectile, Type(std::any_cast<EntityType>(Player_Projectile)));
+    playerProjectile = registry.addComponent(
+        playerProjectile,
+        Size(std::make_pair(
+            68.5 / registry.getComponent(playerProjectile, Renderer{}).getRenderer().getLocalBounds().width,
+            21.25 / registry.getComponent(playerProjectile, Renderer{}).getRenderer().getLocalBounds().height)));
+
+    Position playerProjectilePos = registry.getComponent(playerProjectile, Position{});
+    std::pair<int, int> pairPos = playerProjectilePos.getPosition();
+    std::cout << "Player projectile created pos: " << pairPos.first << " " << pairPos.second << '\n';
+}
+
+void createHud(Registry& registry, int id, int dataScore)
+{
+    std::cout << "NEW_HUD" << std::endl;
+    Entity hud = registry.createEntityWithID(id);
+    ScorePoint score{};
+
+    score.setScorePoint(dataScore);
+    hud = registry.addComponent(hud, Type(std::any_cast<EntityType>(HUD)));
+    hud = registry.addComponent(hud, score);
+    registry.setEntity(hud, id);
+}
+
+void createScore(Registry& registry, int dataScore)
+{
+    if (registry.hasEntityType(HUD)) {
+        std::cout << "SCORE " << std::endl;
+        Entity score = registry.getScore();
+        ID scoreId = registry.getComponent(score, ID{});
+        ScorePoint& scorePoints = registry.getComponent(score, ScorePoint{});
+        scorePoints.setScorePoint(dataScore);
+        registry.setEntity(score, scoreId);
+    } else {
+        std::cout << "[LOG] NO SCORE" << std::endl;
+    }
+}
+
 void handleReceive(
     Registry& registry,
     const boost::system::error_code& error,
@@ -61,138 +182,43 @@ void handleReceive(
         TransferData receivedData{.command = EMPTY};
         std::memcpy(&receivedData, buffer, sizeof(receivedData));
 
-        if (receivedData.command == NEW_POS) {
-            int id = receivedData.args[0];
-            int xPos = receivedData.args[1];
-            int yPos = receivedData.args[2];
-
-            try {
-                Entity entity = registry.getEntity(id);
-                if (registry.hasComponent(entity, Position{})) {
-                    Position& entityPos = registry.getComponent(entity, Position{});
-                    entityPos.setPosition(std::make_pair(xPos, yPos));
-                    registry.setEntity(entity, id);
-                }
-            } catch (const std::exception& e) {
-                std::cerr << "Exception in handleReceive: " << e.what() << std::endl;
-            }
-        } else if (receivedData.command == NEW_HEALTH) {
-            int id = receivedData.args[0];
-            int health = receivedData.args[1];
-
-            try {
-                Entity entity = registry.getEntity(id);
-                if (registry.hasComponent(entity, HealthPoint{})) {
-                    HealthPoint& entityPos = registry.getComponent(entity, HealthPoint{});
-                    entityPos.setHealthPoint(health);
-                    registry.setEntity(entity, id);
-                }
-            } catch (const std::exception& e) {
-                std::cerr << "Exception in handleReceive: " << e.what() << std::endl;
-            }
-        } else if (receivedData.command == NEW_PLAYER) {
-            int id = receivedData.args[0];
-            int xPos = receivedData.args[1];
-            int yPos = receivedData.args[2];
-            int healthPoint = receivedData.args[3];
-
-            if (registry.hasEntity(id))
-                return;
-            Entity player = registry.createEntityWithID(id);
-            player = registry.addComponent(player, Position(std::make_pair(xPos, yPos)));
-            player = registry.addComponent(player, HealthPoint(healthPoint));
-            player = registry.addComponent(player, Renderer("../Client/assets/Cars/189.png"));
-            player = registry.addComponent(player, Type(std::any_cast<EntityType>(Player)));
-            player = registry.addComponent(
-                player,
-                Size(std::make_pair(
-                    103 / registry.getComponent(player, Renderer{}).getRenderer().getLocalBounds().width,
-                    56.25 / registry.getComponent(player, Renderer{}).getRenderer().getLocalBounds().height)));
-            registry.setEntity(player, id);
-        } else if (receivedData.command == NEW_ENEMY) {
-            int id = receivedData.args[0];
-            int xPos = receivedData.args[1];
-            int yPos = receivedData.args[2];
-
-            Entity enemy = registry.createEntityWithID(id);
-            enemy = registry.addComponent(enemy, Position(std::make_pair(xPos, yPos)));
-            enemy = registry.addComponent(enemy, Renderer("../Client/assets/Cars/cars/190.png"));
-            enemy = registry.addComponent(enemy, Type(std::any_cast<EntityType>(Enemy)));
-            enemy = registry.addComponent(
-                enemy,
-                Size(std::make_pair(
-                    103 / registry.getComponent(enemy, Renderer{}).getRenderer().getLocalBounds().width,
-                    56.25 / registry.getComponent(enemy, Renderer{}).getRenderer().getLocalBounds().height)));
-
-            Position enemyPos = registry.getComponent(enemy, Position{});
-            std::pair<int, int> pairPos = enemyPos.getPosition();
-
-            std::cout << "Enemy created pos: " << pairPos.first << " " << pairPos.second << '\n';
-        } else if (receivedData.command == NEW_BOSS) {
-            int id = receivedData.args[0];
-            int xPos = receivedData.args[1];
-            int yPos = receivedData.args[2];
-
-            Entity enemy = registry.createEntityWithID(id);
-            enemy = registry.addComponent(enemy, Position(std::make_pair(xPos, yPos)));
-            enemy = registry.addComponent(enemy, Renderer("../Client/assets/Boss/v-police.png"));
-            enemy = registry.addComponent(enemy, Type(std::any_cast<EntityType>(Enemy)));
-            enemy = registry.addComponent(enemy, Size(std::make_pair(2, 2)));
-
-            Position enemyPos = registry.getComponent(enemy, Position{});
-            std::pair<int, int> pairPos = enemyPos.getPosition();
-
-            std::cout << "Enemy created pos: " << pairPos.first << " " << pairPos.second << '\n';
-        } else if (receivedData.command == PLAYER_PROJECTILE) {
-            std::cout << "player projectile created" << std::endl;
-            int id = receivedData.args[0];
-            int xPos = receivedData.args[1];
-            int yPos = receivedData.args[2];
-
-            Entity playerProjectile = registry.createEntityWithID(id);
-            playerProjectile = registry.addComponent(playerProjectile, Position(std::make_pair(xPos, yPos)));
-            playerProjectile = registry.addComponent(playerProjectile, Renderer("../Client/assets/Cars/movement parts/thruster/flame.png"));
-            playerProjectile = registry.addComponent(playerProjectile, Type(std::any_cast<EntityType>(Player_Projectile)));
-            playerProjectile = registry.addComponent(
-                playerProjectile,
-                Size(std::make_pair(
-                    68.5 / registry.getComponent(playerProjectile, Renderer{}).getRenderer().getLocalBounds().width,
-                    21.25 / registry.getComponent(playerProjectile, Renderer{}).getRenderer().getLocalBounds().height)));
-
-            Position playerProjectilePos = registry.getComponent(playerProjectile, Position{});
-            std::pair<int, int> pairPos = playerProjectilePos.getPosition();
-            std::cout << "player projectile created created pos: " << pairPos.first << " " << pairPos.second << '\n';
-        } else if (receivedData.command == DELETE) {
-            registry.deleteById(receivedData.args[0]);
-        } else if (receivedData.command == PLAY_BOOM_ENEMIES) {
-            music.boomEnemies.play();
-        } else if (receivedData.command == NEW_HUD) {
-            std::cout << "NEW_HUD" << std::endl;
-            int id = receivedData.args[0];
-            int data_score = receivedData.args[1];
-
-            Entity entity_score = registry.createEntityWithID(id);
-            ScorePoint score;
-            score.setScorePoint(data_score);
-            entity_score = registry.addComponent(entity_score, Type(std::any_cast<EntityType>(HUD)));
-            entity_score = registry.addComponent(entity_score, score);
-            registry.setEntity(entity_score, id);
-        } else if (receivedData.command == SCORE) {
-            if (registry.hasEntityType(HUD)) {
-                std::cout << "SCORE " << std::endl;
-                Entity score = registry.getScore();
-                ID score_id = registry.getComponent(score, ID{});
-                ScorePoint& score_points = registry.getComponent(score, ScorePoint{});
-
-                score_points.setScorePoint(receivedData.args[0]);
-
-                registry.setEntity(score, score_id);
-            } else {
+        switch (receivedData.command) {
+            case NEW_POS:
+                updatePosition(registry, receivedData.args[0], receivedData.args[1], receivedData.args[2]);
+                break;
+            case NEW_HEALTH:
+                updateHealth(registry, receivedData.args[0], receivedData.args[1]);
+                break;
+            case NEW_PLAYER:
+                createPlayer(registry, receivedData.args[0], receivedData.args[1], receivedData.args[2], receivedData.args[3]);
+                break;
+            case NEW_ENEMY:
+                createEnemy(registry, receivedData.args[0], receivedData.args[1], receivedData.args[2]);
+                break;
+            case NEW_BOSS:
+                createBoss(registry, receivedData.args[0], receivedData.args[1], receivedData.args[2]);
+                break;
+            case PLAYER_PROJECTILE:
+                createPlayerProjectile(registry, receivedData.args[0], receivedData.args[1], receivedData.args[2]);
+                break;
+            case DELETE:
+                registry.deleteById(receivedData.args[0]);
+                break;
+            case PLAY_BOOM_ENEMIES:
+                music.boomEnemies.play();
+                break;
+            case NEW_HUD:
+                createHud(registry, receivedData.args[0], receivedData.args[1]);
+                break;
+            case SCORE:
+                createScore(registry, receivedData.args[0]);
+                break;
+            default:
                 std::cout << "[LOG] NO SCORE" << std::endl;
-            }
-        } else if (error) {
-            std::cerr << "Error in handleReceive: " << error.message() << std::endl;
+                break;
         }
+    } else if (error) {
+        std::cerr << "Error in handleReceive: " << error.message() << std::endl;
     }
 }
 
@@ -220,7 +246,7 @@ void sendToServer(boost::asio::ip::udp::socket& socket, const std::string& msg, 
         ipAdress.setUserInput("127.0.0.1");
     }
     boost::asio::ip::udp::endpoint receiverEndpoint(boost::asio::ip::address::from_string(ipAdress.getUserInput()), 7171);
-    socket.async_send_to(boost::asio::buffer(buffer), receiverEndpoint, [](const boost::system::error_code& ec, std::size_t bytes_transferred) {
+    socket.async_send_to(boost::asio::buffer(buffer), receiverEndpoint, [](const boost::system::error_code& ec, std::size_t /*bytes_transferred*/) {
         if (ec) {
             std::cerr << "Send error: " << ec.message() << std::endl;
         } else {
@@ -240,7 +266,7 @@ void CommandsToServer::asyncReceive(Registry& registry, Music& music)
     });
 }
 
-std::future<void> CommandsToServer::sendToServerAsync(std::string msg, IpAdress& ipAdress)
+std::future<void> CommandsToServer::sendToServerAsync(const std::string& msg, IpAdress& ipAdress)
 {
     std::shared_ptr<std::promise<void>> promise = std::make_shared<std::promise<void>>();
 
